3953db5648063786fdcf6808ed0e4644ba28edd8
Dev Uttam <80285567+DevAavash2020@users.noreply.github.com>
2021-03-09 16:37:03 +0000

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", (request,response)=>{
+    response.sendFile(__dirname + "/index.html");
+})
+
+app.post("/index.html",(req,res)=>{
+    const weight = Number(req.body.num1);
+    const height = Number(req.body.num2);
+
+    const result = weight/(height*height);
+
+    res.send("The bmi is " + result);
+})
+app.listen("3000", function (){
+    console.log("listening to the port 3000")
+})
